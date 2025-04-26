6186cbd4fe2cf2d2bc3a3f5eaac9b3d2b5131149
melleeeee <ajtmari22@src.pshs.edu.ph>
2025-04-26 10:34:02 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const tileSize = 25; // Size of each block in the maze
+
+// Maze structure: 1 = wall, 0 = path
+const maze = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,0,1],
+  [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1],
+  [1,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,1],
+  [1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,0,1],
+  [1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+  [1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1],
+  [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1],
+  [1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,0,1],
+  [1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1,0,1],
+  [1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,0,1],
+  [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1],
+  [1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1],
+  [1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+  [1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1],
+  [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1],
+  [1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1],
+  [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
+
+// Player position
+let player = {
+  x: 1,
+  y: 1
+};
+
+function drawMaze() {
+  for (let row = 0; row < maze.length; row++) {
+    for (let col = 0; col < maze[row].length; col++) {
+      if (maze[row][col] === 1) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
+      } else {
+        ctx.fillStyle = "black";
+        ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
+      }
+    }
+  }
+}
+
+function drawPlayer() {
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.arc(player.x * tileSize + tileSize/2, player.y * tileSize + tileSize/2, tileSize/3, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function gameLoop() {
+  drawMaze();
+  drawPlayer();
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp" && maze[player.y - 1][player.x] === 0) {
+    player.y--;
+  }
+  if (e.key === "ArrowDown" && maze[player.y + 1][player.x] === 0) {
+    player.y++;
+  }
+  if (e.key === "ArrowLeft" && maze[player.y][player.x - 1] === 0) {
+    player.x--;
+  }
+  if (e.key === "ArrowRight" && maze[player.y][player.x + 1] === 0) {
+    player.x++;
+  }
+  gameLoop();
+});
+
+// Initial draw
+gameLoop();
